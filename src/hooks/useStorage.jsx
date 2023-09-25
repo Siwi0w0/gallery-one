a215d6bdc4e1fcbd7567e72b9c377e1394e5f762
Siwi0w0 <yufei.sun64@gmail.com>
2023-09-25 18:07:36 +0800

    fix some bugs

--- src/hooks/useStorage.jsx
+++ src/hooks/useStorage.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
-import { ref } from 'firebase/storage';
+import { ref, uploadBytesResumable } from 'firebase/storage';
 import { storage } from '../firebase/config';
 import { v4 as uuidv4 } from 'uuid';
-import { uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const useStorage = () => {
     const [progress, setProgress] = useState(0);
@@ -11,44 +10,43 @@ const useStorage = () => {
 
     const startUpload = (file) => {
         //upload file to storage
-        if(!file) return;
-    }
+        if(!file) {return;}
 
-    const fileId = uuidv4();
-    const formatFile = file.type.split('/')[1];
+        //create random id
+        const fileId = uuidv4();
+        const formatFile = file.type.split('/')[1];
+        console.log(formatFile);
+      
+        const storageRef = ref(storage, `images/${fileId}.${formatFile}`);
 
-    // Create a reference to 'images/fileId.formatFile'
-    const storageRef = ref(storage, `images/${fileId}.${formatFile}`);
+        uploadTask = uploadBytesResumable(storageRef, file);
+       
 
-    // Upload file and metadata to the object 'images/fileId.formatFile'
-    const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on('state_changed', 
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 
-    uploadTask.on('state_changed', 
-  (snapshot) => {
-    // Observe state change events such as progress, pause, and resume
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-    }
-  }, 
-  (error) => {
-    // Handle unsuccessful uploads
-  }, 
-  () => {
-    // Handle successful uploads on complete
-    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
-    });
+          setProgress(progress);
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        }, 
+        (error) => {
+          setError(error);
+        }, () => {
+          // Handle successful uploads on complete
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+          });
   }
 );
+};
 
     return(
         progress, error, url, startUpload
